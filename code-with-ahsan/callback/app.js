712ae511862e668a8c8e8b841f9e68f6a01e6fbb
Zaib Ali <Zaibismail92@gmail.com>
2026-01-01 22:48:06 +0500

    Another callback example

--- code-with-ahsan/callback/app.js
+++ code-with-ahsan/callback/app.js
@@ -1,17 +1,45 @@
+// const getUser = (callbackFn) => {
+//     fetch('https://randomuser.me/api')
+//         .then(res => res.json())
+//         .then(res => {
+//             callbackFn(res);
+//         })
+// }
+
+// const main = () => {
+//     let data;
+//     getUser(function(response) {
+//         data = response;
+//         console.log(data);
+//     })
+// }
+
+// main();
+
+
 const getUser = (callbackFn) => {
-    fetch('https://randomuser.me/api')
-        .then(res => res.json())
-        .then(res => {
-            callbackFn(res);
-        })
+    setTimeout(() =>{
+        const userInfo = {
+            user: 'Zayb Ali'
+        };
+        callbackFn(userInfo)
+    },3000)
+}
+
+const getUserSync = () => {
+    const userInfo = {
+        user: 'Zayb Ali'
+    }
+    return userInfo;
 }
 
 const main = () => {
-    let data;
-    getUser(function(response) {
-        data = response;
-        console.log(data);
-    })
+    getUser(function(userInfoObj) {
+        console.log('async', userInfoObj)
+    });
 }
+
+const userInfoObj = getUserSync();
+console.log('sync', userInfoObj);
 
 main();
