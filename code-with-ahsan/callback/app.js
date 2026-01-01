a2f0a023d9cb4a3e6c517d011831caf0801b777a
Zaib Ali <Zaibismail92@gmail.com>
2026-01-01 22:57:41 +0500

    Sync function

--- code-with-ahsan/callback/app.js
+++ code-with-ahsan/callback/app.js
@@ -1,45 +1,60 @@
-// const getUser = (callbackFn) => {
-//     fetch('https://randomuser.me/api')
-//         .then(res => res.json())
-//         .then(res => {
-//             callbackFn(res);
-//         })
-// }
+//  Synchronous 
 
-// const main = () => {
-//     let data;
-//     getUser(function(response) {
-//         data = response;
-//         console.log(data);
-//     })
-// }
-
-// main();
-
-
-const getUser = (callbackFn) => {
-    setTimeout(() =>{
-        const userInfo = {
-            user: 'Zayb Ali'
-        };
-        callbackFn(userInfo)
-    },3000)
-}
-
-const getUserSync = () => {
-    const userInfo = {
-        user: 'Zayb Ali'
+const getUser = () => {
+    return {
+        user: "Zayb Ali"
     }
-    return userInfo;
 }
 
 const main = () => {
-    getUser(function(userInfoObj) {
-        console.log('async', userInfoObj)
-    });
+    const userInfo = getUser();
+    console.log({userInfo});
 }
 
-const userInfoObj = getUserSync();
-console.log('sync', userInfoObj);
-
 main();
+
+// // const getUser = (callbackFn) => {
+// //     fetch('https://randomuser.me/api')
+// //         .then(res => res.json())
+// //         .then(res => {
+// //             callbackFn(res);
+// //         })
+// // }
+
+// // const main = () => {
+// //     let data;
+// //     getUser(function(response) {
+// //         data = response;
+// //         console.log(data);
+// //     })
+// // }
+
+// // main();
+
+
+// const getUser = (callbackFn) => {
+//     setTimeout(() =>{
+//         const userInfo = {
+//             user: 'Zayb Ali'
+//         };
+//         callbackFn(userInfo)
+//     },3000)
+// }
+
+// const getUserSync = () => {
+//     const userInfo = {
+//         user: 'Zayb Ali'
+//     }
+//     return userInfo;
+// }
+
+// const main = () => {
+//     getUser(function(userInfoObj) {
+//         console.log('async', userInfoObj)
+//     });
+// }
+
+// const userInfoObj = getUserSync();
+// console.log('sync', userInfoObj);
+
+// main();
