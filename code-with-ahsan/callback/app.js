10471181ba518495a119bcdc1543ba00c1b74ae5
Zaib Ali <Zaibismail92@gmail.com>
2026-01-03 00:23:49 +0500

    getUser function to use fetch API for asynchronous data retrieval

--- code-with-ahsan/callback/app.js
+++ code-with-ahsan/callback/app.js
@@ -43,73 +43,45 @@
 // main();
 
 
-const getUser = (getUserSuccess) => {
-    setTimeout(() => {
-        getUserSuccess({
-            name: 'Zayb'
+// const getUser = (getUserSuccess) => {
+//     setTimeout(() => {
+//         getUserSuccess({
+//             name: 'Zayb'
+//         })
+//     }, 1500) 
+// }
+
+// const getEvent = (getEventSuccess) => {
+//     setTimeout(() => {
+//         getEventSuccess({
+//             name: 'Portfolio building'
+//         })
+//     }, 500) 
+// }
+
+// getUser(function(user){
+//     console.log({user});
+// })
+
+// getEvent(function(event){
+//     console.log({event});
+// })
+
+
+const getUser = (callbackFn) => {
+    fetch('https://randomuser.me/api')
+        .then(res => res.json())
+        .then(res => {
+            callbackFn(res);
         })
-    }, 1500) 
 }
 
-const getEvent = (getEventSuccess) => {
-    setTimeout(() => {
-        getEventSuccess({
-            name: 'Portfolio building'
-        })
-    }, 500) 
+const main = () => {
+    let data;
+    getUser(function(response) {
+    data = response;
+    console.log(data);
+    })
 }
 
-getUser(function(user){
-    console.log({user});
-})
-
-getEvent(function(event){
-    console.log({event});
-})
-
-
-// // const getUser = (callbackFn) => {
-// //     fetch('https://randomuser.me/api')
-// //         .then(res => res.json())
-// //         .then(res => {
-// //             callbackFn(res);
-// //         })
-// // }
-
-// // const main = () => {
-// //     let data;
-// //     getUser(function(response) {
-// //         data = response;
-// //         console.log(data);
-// //     })
-// // }
-
-// // main();
-
-
-// const getUser = (callbackFn) => {
-//     setTimeout(() =>{
-//         const userInfo = {
-//             user: 'Zayb Ali'
-//         };
-//         callbackFn(userInfo)
-//     },3000)
-// }
-
-// const getUserSync = () => {
-//     const userInfo = {
-//         user: 'Zayb Ali'
-//     }
-//     return userInfo;
-// }
-
-// const main = () => {
-//     getUser(function(userInfoObj) {
-//         console.log('async', userInfoObj)
-//     });
-// }
-
-// const userInfoObj = getUserSync();
-// console.log('sync', userInfoObj);
-
-// main();
+main();
