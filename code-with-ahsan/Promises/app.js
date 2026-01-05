b3ba1116473eaf09223e24f0a4a26560aca9b198
Zaib Ali <Zaibismail92@gmail.com>
2026-01-05 20:17:29 +0500

    Refactor doHomework to restore parallel execution of book requests and improve logging

--- code-with-ahsan/Promises/app.js
+++ code-with-ahsan/Promises/app.js
@@ -17,41 +17,41 @@ const yaseen = {
 
 const ahmed = {
     doHomework() {
-        // const promises = Promise.all([
-        //     yaseen.requestMathsBook(),
-        //     yaseen.requestPhysicsBook()
-        // ]);
-        // console.time('executing parallel')
-        // promises.then(([mathsBook, physicsBook]) =>{
-        //     console.timeEnd('executing parallel')
-        //     console.log('recieved the book ', mathsBook);
-        //     console.log('look at homework instructions ', mathsBook);
-        //     console.log('write in the notebook ', mathsBook);
-        //     console.log('recieved the book ', physicsBook);
-        //     console.log('look at homework instructions ', physicsBook);
-        //     console.log('write in the notebook ', physicsBook);
-        // }).catch((err) => {
-        //     console.error(err);
-        // })
+        const promises = Promise.all([
+            yaseen.requestMathsBook(),
+            yaseen.requestPhysicsBook()
+        ]);
+        console.time('executing parallel')
+        promises.then(([mathsBook, physicsBook]) =>{
+            console.timeEnd('executing parallel')
+            console.log('recieved the book ', mathsBook);
+            console.log('look at homework instructions ', mathsBook);
+            console.log('write in the notebook ', mathsBook);
+            console.log('recieved the book ', physicsBook);
+            console.log('look at homework instructions ', physicsBook);
+            console.log('write in the notebook ', physicsBook);
+        }).catch((err) => {
+            console.error(err);
+        })
 
 
-        console.time('executing sequential')
-        yaseen.requestMathsBook()
-            .then((bookName) => {
-                console.log('recieved the book ', bookName);
-                console.log('look at homework instructions ', bookName);
-                console.log('write in the notebook ', bookName);
-                return yaseen.requestPhysicsBook();
-            }).then((bookName)=>{
-                console.log('recieved the book ', bookName);
-                console.log('look at homework instructions ', bookName);
-                console.log('write in the notebook ', bookName);
-            }).then(()=>{
-                console.timeEnd('executing sequential')    
-                console.log('Finished homework');
-            }).catch((err)=>{
-                console.error(err);
-            })
+        // console.time('executing sequential')
+        // yaseen.requestMathsBook()
+        //     .then((bookName) => {
+        //         console.log('recieved the book ', bookName);
+        //         console.log('look at homework instructions ', bookName);
+        //         console.log('write in the notebook ', bookName);
+        //         return yaseen.requestPhysicsBook();
+        //     }).then((bookName)=>{
+        //         console.log('recieved the book ', bookName);
+        //         console.log('look at homework instructions ', bookName);
+        //         console.log('write in the notebook ', bookName);
+        //     }).then(()=>{
+        //         console.timeEnd('executing sequential')    
+        //         console.log('Finished homework');
+        //     }).catch((err)=>{
+        //         console.error(err);
+        //     })
         console.table('1')
         console.table('2')
         console.table('3')
